f4dab84695d34a0623972c6dbebb378a91b8dfa1
Roey Biran <roeybiran@icloud.com>
2021-08-09 19:35:25 +0300

    fix index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,6 @@ export default function Home(
                   firstName: project.student.firstName,
                   lastName: project.student.lastName,
                 }}
-                showLocation={false}
               />
             ))}
           </Grid>
